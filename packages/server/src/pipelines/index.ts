91fcd74119147d9e1131294a4df9aa6264d40804
Anthony Chen <gamertonystuck@gmail.com>
2026-03-04 03:35:53 -0500

    agentic feature added to discord
    limited feature, /agent prompt currently only shows up in servers

--- packages/server/src/pipelines/index.ts
+++ packages/server/src/pipelines/index.ts
@@ -1,5 +1,20 @@
 export type { PipelineRecord, PipelineType, PipelineStatus, PipelineConfig, DiscordConfig, TelegramConfig, LineConfig } from './types.js';
-export { runAgentForPipelineAsync, splitMessage, type ApprovalFn, type SessionRecoveryFn } from './runner.js';
+export {
+  runAgentForPipelineAsync,
+  runAgentStepAsync,
+  executeApprovedProposalsAsync,
+  denyProposalsAsync,
+  runWithBatchApprovalAsync,
+  runAgenticTaskAsync,
+  splitMessage,
+  type ApprovalFn,
+  type SessionRecoveryFn,
+  type Proposal,
+  type PendingApproval,
+  type BatchApprovalSendFn,
+  type AgenticApprovalFn,
+  type AgenticCheckpointFn,
+} from './runner.js';
 export { DiscordPipeline } from './discord.js';
 export { TelegramPipeline } from './telegram.js';
 export { LinePipeline } from './line.js';
